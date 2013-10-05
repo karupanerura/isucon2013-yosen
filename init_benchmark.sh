dfcb709a50e2a87690643c5e94f40f6f0826388c
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2013-10-05 15:44:47 +0900

    dbひつよう

--- init_benchmark.sh
+++ init_benchmark.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-mysqldump -uisucon --single-transaction --compress --order-by-primary --opt > /home/isucon/logs/mysqldump/$(date +'%Y%m%d_%H%M').sql
+mysqldump -uisucon --single-transaction --compress --order-by-primary --opt isucon > /home/isucon/logs/mysqldump/$(date +'%Y%m%d_%H%M').sql
 cat /home/isucon/webapp/config/warmup.sql | mysql -uisucon isucon
 cat /home/isucon/webapp/config/alter.sql | mysql -uisucon isucon
