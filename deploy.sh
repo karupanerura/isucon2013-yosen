c76e4d7a0703211a3f0b1a02be21763d24fe2b21
karupanerura <karupa@cpan.org>
2013-10-05 10:48:25 +0900

    set -x

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,4 @@ set -x
 REMOTE_SERVER="ec2-54-238-158-189.ap-northeast-1.compute.amazonaws.com"
 REMOTE_APP_DIR="webapp"
 
-REMOTE_COMMAND=<EOF
-flock -x /tmp/isucon.deploy.lock zsh -c "cd $REMOTE_APP_DIR; git pull; ./restart.sh"
-EOF
-
-ssh $REMOTE_SERVER $REMOTE_COMMAND
+ssh isucon@$REMOTE_SERVER flock -x /tmp/isucon.deploy.lock bash -xc 'cd $REMOTE_APP_DIR; git pull; ./restart.sh'
