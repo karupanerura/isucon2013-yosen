5fc170758d296e6a397d7d1f5dfea54e02f247ea
Kenta, Kobayashi <kentafly88@gmail.com>
2013-10-05 14:17:35 +0900

    init benchmarkをdeploy.shで走らせて、schema打つ手間を減らす

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 
-ssh -t isucon@ec2-54-238-158-189.ap-northeast-1.compute.amazonaws.com flock -x /tmp/isucon.deploy.lock bash -c 'pwd > /dev/null; cd webapp; git pull >> ~/deploy.log 2>&1; sudo cp mysql/my.cnf /usr/my.cnf >> ~/deploy.log 2>&1; ./restart.sh'
+ssh -t isucon@ec2-54-238-158-189.ap-northeast-1.compute.amazonaws.com flock -x /tmp/isucon.deploy.lock bash -c 'pwd > /dev/null; cd webapp; git pull >> ~/deploy.log 2>&1; sudo cp mysql/my.cnf /usr/my.cnf >> ~/deploy.log 2>&1;  ~/webapp/init_benchmark.sh;./restart.sh'
