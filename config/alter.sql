b8aa42289e255207be19b6f5f16dd2c5b5ca6f17
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2013-10-05 15:56:06 +0900

    miss

--- config/alter.sql
+++ config/alter.sql
@@ -5,7 +5,7 @@ ALTER TABLE memos_new
     ADD COLUMN `username` varchar(20) NOT NULL AFTER user, -- FOR CACHE
     DROP COLUMN `updated_at`,
     ADD INDEX is_private_created_at (is_private, created_at),
-    ADD INDEX user_created_at (is_private, created_at);
+    ADD INDEX user_created_at (user, created_at);
 INSERT INTO memos_new
     (id, user, username, content, is_private, created_at, updated_at)
     SELECT memos.id, memos.user, users.username, memos.content, memos.is_private, memos.created_at, memos.updated_at FROM memos INNER JOIN users ON memos.user = users.id;
