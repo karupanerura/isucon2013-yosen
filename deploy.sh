12813dcf8ae5b5d78865e44afb10f363508ec2d7
Kenta, Kobayashi <kentafly88@gmail.com>
2013-10-05 16:33:07 +0900

    amazonに行って、deploy-local.sh を叩くように変更

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-set -e
-set -x
-
-ssh -t isucon@ec2-54-238-158-189.ap-northeast-1.compute.amazonaws.com flock -x /tmp/isucon.deploy.lock bash -c 'pwd > /dev/null; cd webapp; ./deploy-local.sh >> ~/deploy.log 2>&1'
