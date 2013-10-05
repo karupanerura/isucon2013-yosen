4f6992cbbf226051330edc35bd062755a468fd57
karupanerura <karupa@cpan.org>
2013-10-05 11:13:58 +0900

    ooopsssss!!!!

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ exec >> ~/run.log
 exec 2>&1
 
 cd perl
-./env.sh carton install
-./env.sh carton exec plackup -s Starman -p 5000 -E prod --workers 10 --disable-keepalive app.psgi
+../env.sh carton install
+../env.sh carton exec plackup -s Starman -p 5000 -E prod --workers 10 --disable-keepalive app.psgi
