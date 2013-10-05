551593dab9dc704c6aaa8e7954beb44737a8baeb
karupanerura <karupa@cpan.org>
2013-10-05 15:20:06 +0900

    worker数を更に倍にしてみる

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ exec 2>&1
 
 cd perl
 ../env.sh carton install
-../env.sh carton exec plackup -s Starlet -p 5000 -E prod --max-workers=40 --keepalive-timeout=300 --max-keepalive-reqs=1000 --max-reqs-per-child=10240 --spawn-interval=2 app.psgi
+../env.sh carton exec plackup -s Starlet -p 5000 -E prod --max-workers=80 --keepalive-timeout=300 --max-keepalive-reqs=1000 --max-reqs-per-child=10240 --spawn-interval=2 app.psgi
